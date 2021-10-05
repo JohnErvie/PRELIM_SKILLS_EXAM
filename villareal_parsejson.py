e311475a05b2cac8f8ea5f01802441b2cfcd2eba
ejtvillareal@tip.edu.ph <qejtvillareal@tip.edu.ph>
2021-10-05 03:23:06 +0000

    parsejson

--- villareal_parsejson.py
+++ villareal_parsejson.py
@@ -1,4 +1,10 @@
 import json
-import yaml
+import csv
 
-with open('covid_cases.json','r') as jsonFile:
+with open('covid_cases.json','r') as f:
+    #print(f)
+    covid = json.load(f)
+
+with open('test.csv', 'w') as f:
+    for key in covid.keys():
+        f.write("%s,%s\n"%(key,covid[key]))
